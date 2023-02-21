e636ba834a3c428f6dd06feb6fbc660dc036a82d
Lucas Emanuel <88749179+lucasemanueldev@users.noreply.github.com>
2023-02-21 16:33:53 -0300

    Cobra e Tela

--- Jogo da Cobrinha/jogo.js
+++ Jogo da Cobrinha/jogo.js
@@ -14,7 +14,75 @@ window.onload = function(){
     velX = 0 //velocidade horizontal da cobra
     velY = 0 //velocida vertical da cobra
     //tamanho da grade
-    grade = 20
+    grade = 40
     //tamanho inicial da cobra
     tam = 3
+
+    //ficar chamando a função jogo de 100 em 100 milisegundos
+    setInterval(jogo, 100)
+
+    //controles do game
+    document.addEventListener("keydown",function(e){
+        switch(e.keyCode){
+            //seta direita
+            case 39:
+                velX = 1;
+                velY = 0;
+                break;
+            //seta esquerda
+            case 37:
+                velX = -1;
+                velY = 0;
+                break;
+            //seta cima
+            case 38:
+                velX = 0;
+                velY = -1;
+                break;
+            //seta baixo
+            case 40:
+                velX = 0;
+                velY = 1;
+                break;
+        }
+    })
 }
+function jogo(){
+    //configurações de tela
+    contexto.fillStyle = "#201b2c"
+    //distância borda horizontal, distância borda vertical, largura, altura
+    contexto.fillRect(0,0, 800, 800)
+
+    //cobra se mexendo
+    positionX += velX
+    positionY += velY
+
+    //espelhar a cobra
+    if(positionX < 0){
+        positionX = grade
+    }
+    if(positionX > grade){
+        positionX = 0
+    }
+    if(positionY < 0){
+        positionY = grade
+    }
+    if(positionY > grade){
+        positionY = 0
+    }
+
+    //posição da cobra
+    cobra.push({x: positionX, y: positionY})
+    //configurações da cobra
+    contexto.fillStyle = "#8b0000";
+    for(let i = 0; i < cobra.length; i++){
+        contexto.fillRect(cobra[i].x*grade, cobra[i].y*grade, grade-1, grade-1)
+    }
+
+    //apagar cobra
+    while(cobra.length > tam){
+        cobra.shift()
+    }
+
+}
+
